135d01cec77a4f4c2071cb64d60848e8ef20bf5a
ChenBo <30582761+ChenBo233@users.noreply.github.com>
2018-04-17 16:16:00 +0800

    es6 class 重写
    es6 class 重写

--- qiniuUploader.js
+++ qiniuUploader.js
@@ -2,132 +2,119 @@
  * Created by chenbo on 2017/9/18.
  *
  * 1.引入： import Uploader from '@/qiniuUploader';
- * 2.初始化参数，获取七牛token(token、tokenPort必须传一个)： Uploader.init();
- * 3.上传：Uploader.upload().then((key)=>{上传成功返回七牛文件key});
+ * 2.实例化 let uploader = new Uploader(token、tokenPort必须传一个);
+ * 3.上传：uploader.upload().then((key)=>{上传成功返回七牛文件key});
  *
  * 由于每个项目需求和UI不同，所以尽量解耦，去掉了加载和上传的动画，需要的同学可以在回调中加入，或者修改源码统一实现
+ * 最新修改2018-3-30 16:58
  */
 import axios from 'axios';
-
-let el = '';
-
-let config = {
-    token: '', //七牛token
-    tokenPort: 'http://10.40.11.70:41002/dafystore_app/v1/qiniu/uptoken', //获取七牛token的接口，和token二选一
-    accept: 'image/png,image/gif,image/jpg,image/jpeg', //可上传文件的类型
-    begin: '', //开始上传回调
-    succeed: '', //上传成功回调，等同于then
-    fail: '' //上传失败回调，等同于catch
-};
-
-let init = (options) => {
-    config = Object.assign(config, options);
-    try {
-        if (!config.token && !config.tokenPort) {
-            throw '没有传入token或者tokenPort';
-        } else if (config.tokenPort) {
-            getToken();
+class  Uploader {
+    constructor (options) {
+        this.qiniuUploadUrl = '';
+        this.el = '';
+        this.config = Object.assign({
+            token: '', //七牛token
+            tokenPort: '', //获取七牛token的接口，和token二选一
+            accept: 'image/png,image/gif,image/jpg,image/jpeg', //可上传文件的类型,
+            serversArea: 'z2',//服务器地区 z0华东 z1华北 z2华南 na0北美 as0东南亚
+            begin: '', //开始上传回调
+            succeed: '', //上传成功回调，等同于then
+            fail: '' //上传失败回调，等同于catch
+        }, options);
+        this.init();
+    }
+    init () {
+        this.qiniuUploadUrl = '//upload-' + this.config.serversArea + '.qiniup.com';
+        if (!this.config.token) {
+            this.getToken();
         }
     }
-    catch (err) {
-        console.log(err);
+    getToken () {
+        let url = this.config.tokenPort;
+        axios.post(url)
+            .then((res) => {
+                let ret = res.data;
+                if (ret && ret.uptoken) {
+                    this.config.token = ret.uptoken;
+                } else {
+                    console.log('缺少token');
+                }
+            })
+            .catch((error) => {
+                console.log('获取token失败');
+            });
     }
-};
-
-let getToken = () => {
-    let url = config.tokenPort;
-    axios.post(url)
-        .then(function (res) {
-            let ret = res.data;
-            if (ret && ret.data) {
-                config.token = ret.data;
-            } else {
-                console.log('fail to get token');
-            }
-        })
-        .catch(function (error) {
-            reject(error);
+    selectFile () {
+        return new Promise ((resolve, reject) => {
+            this.el = this.el || document.createElement('input');
+            this.el.type = 'file';
+            this.el.accept = this.config.accept;
+            this.el.click();
+            this.el.onchange = () => {
+                let file = this.el.files[0];
+                if (this.config.accept.indexOf(file.type) < 0) {
+                    reject('不支持的文件类型');
+                    return false;
+                }
+                if (this.el.files.length <= 0) {
+                    reject();
+                    return false;
+                }
+                this.el.value = '';
+                resolve(file);
+            };
         });
-};
-
-let selectFile =  () => {
-    return new Promise ((resolve, reject) => {
-        el = el || document.createElement('input');
-        el.type = 'file';
-        el.accept = config.accept;
-        el.click();
-        el.onchange = () => {
-            let file = el.files[0];
-            if (el.files.length <= 0) {
+    }
+    uplodaFile (file) {
+        return new Promise((resolve, reject) => {
+            if (file.size <=0) {//部分安卓机器，未选择文件就会执行onchange事件，为此判断一下是不是真的选择了文件
                 reject();
                 return false;
             }
-            el.value = '';
-            resolve(file);
-        };
-    });
-};
+            let data = new FormData();
 
-let readFile = (file) => {
-    return new Promise((resolve, reject) => {
-        let fr = new FileReader();
-        fr.readAsDataURL(file);
-        fr.onload = function () {
-            resolve(this.result);
-        }
-    })
-};
+            data.append('token', this.config.token);
+            data.append('file', file);
 
-let uplodaFile = (file) => {
-    return new Promise((resolve, reject) => {
-        let data = new FormData();
-        let qiniuUploadUrl = '';
-        data.append('token', config.token);
-        data.append('file', file);
+            axios.post(this.qiniuUploadUrl, data)
+                .then((response) => {
+                    resolve(response);
+                })
+                .catch((error) => {
+                    if (error.response.status === 401) {
+                        this.getToken();
+                    }
+                    reject(error.response.data.error);
+                });
+        });
+    }
+    upload (options) {
+        return new Promise ((resolve, reject) => {
+            if (!this.config.token) {
+                reject('缺少token');
+                this.getToken();
+                return false;
+            }
+            this.config = Object.assign(this.config, options);
+            this.config.begin && typeof this.config.begin === 'function' && this.config.begin();
+            this.selectFile()
+                .then((file) => {
+                    return this.uplodaFile(file);
+                })
+                .then ((res) => {
+                    let key = res.data.key;
+                    console.log('上传成功');
+                    this.config.succeed && typeof this.config.succeed === 'function' && this.config.succeed(key);
+                    resolve(key);
+                })
+                .catch((err) => {
+                    console.log('上传失败');
+                    this.config.fail && typeof this.config.fail === 'function' && this.config.fail(err);
+                    reject(err);
+                })
+        });
+    }
+}
 
-        if (window.location.protocol === 'https:') {
-            qiniuUploadUrl = 'https://up.qbox.me';
-        } else {
-            qiniuUploadUrl = 'http://up.qiniu.com';
-        }
-        axios.post(qiniuUploadUrl, data)
-            .then((response) => {
-                resolve(response);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-    });
-};
-
-let upload = (options) => {
-    return new Promise ((resolve, reject) => {
-        if (!config.token) {
-            reject('没有找到token，请重试...');
-            getToken();
-            return false;
-        }
-        config = Object.assign(config, options);
-        config.begin && typeof config.begin === 'function' && config.begin();
-        selectFile()
-            .then((file) => {
-                return readFile(file);
-            })
-            .then ((file) => {
-                return uplodaFile(file);
-            })
-            .then ((res) => {
-                let key = res.data.key;
-                config.succeed && typeof config.succeed === 'function' && config.succeed(key);
-                resolve(key);
-            })
-            .catch((err) => {
-                config.fail && typeof config.fail === 'function' && config.fail(err);
-                reject(err);
-            })
-    });
-};
-export default {
-    init: init,
-    upload: upload
-};
+export default Uploader;
